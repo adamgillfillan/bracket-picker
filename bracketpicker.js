3b45555087f0c3b13bebd4d6dc56dd2c2ba4dca1
Adam Gillfillan <agillfi@ncsu.edu>
2017-03-14 22:10:45 -0400

    duke wins first full coin flip sim

--- bracketpicker.js
+++ bracketpicker.js
@@ -5,59 +5,87 @@ const getDivision = (teams, division) => {
     return team.division === division;
   })
 };
-
 const divisions = {
   'east': getDivision(teams, 'East'),
   'west': getDivision(teams, 'West'),
   'south': getDivision(teams, 'South'),
   'midwest': getDivision(teams, 'Midwest'),
 };
+let games = {};
 
-// returns: {home: Villanova, away: Mt. St. Mary's / N Orleans, winner: Villanova}
+const init = () => {
+  games = {
+    64: {
+      east: bm.east,
+      west: bm.west,
+      south: bm.south,
+      midwest: bm.midwest
+    },
+    32: {
+      east: [],
+      west: [],
+      south: [],
+      midwest: []
+    },
+    16: {
+      east: [],
+      west: [],
+      south: [],
+      midwest: []
+    },
+    8: {
+      east: [],
+      west: [],
+      south: [],
+      midwest: []
+    },
+    4: {
+      east: [],
+      west: [],
+      south: [],
+      midwest: []
+    },
+    2: {},
+    champion: {}
+  };
+};
+
 const getWinner = (home, away, strategy) => {
   if (strategy === 'coinflip') {
     const winner = (Math.random() > .5) ? home : away;
-    return {home: home.school, away: away.school, winner: winner.school}
+
+    return {home, away, winner};
   }
 };
 
-let games = {
-  roundof64: {
-    east: [],
-    west: [],
-    south: [],
-    midwest: []
-  },
-  roundof32: {
-    east: [],
-    west: [],
-    south: [],
-    midwest: []
-  },
-  sweet16: {
-    east: [],
-    west: [],
-    south: [],
-    midwest: []
-  },
-  elite8: {
-    east: [],
-    west: [],
-    south: [],
-    midwest: []
-  },
-  final4: {},
-  championship: {}
-};
-const buildFirstRound = () => {
-  for (let division in games.roundof64) {
-    for (let i=0; i < 7; i++) {
-      games.roundof64[division].push(getWinner(bm[division][i], bm[division][bm[division].length - (i + 1)], 'coinflip'));
+const doRound = (round) => {
+  for (let division in games[round]) {
+    for (let i=0; i < (round / 8); i++) {
+      let home, away;
+
+      if (round === 64) {
+        home = games[round][division][i];
+        away = games[round][division][games[round][division].length - (i + 1)];
+      } else {
+        home = games[round][division][i].winner;
+        away = games[round][division][games[round][division].length - (i + 1)].winner;
+      }
+
+      games[round/2][division].push(getWinner(home, away, 'coinflip'));
     }
   }
+};
 
-  console.log(games.roundof64.east);
-  console.log(games.roundof64.west);
+const doFinalFour = () => {
+  doRound(64);
+  doRound(32);
+  doRound(16);
+  doRound(8);
+
+  let home = getWinner(games[4].east[0].winner, games[4].west[0].winner, 'coinflip');
+  let away = getWinner(games[4].south[0].winner, games[4].midwest[0].winner, 'coinflip');
+
+  games[2] = {home, away};
 };
 
 // bracketmanager
@@ -69,9 +97,13 @@ const bm = module.exports = {
   midwest: divisions.midwest,
 
   createBracket: () => {
-    // console.log('teams', teams);
-    buildFirstRound(teams);
-    return teams;
+    init();
+    doFinalFour();
+
+    games.champion = getWinner(games[2].home.winner, games[2].away.winner, 'coinflip');
+
+    console.log(games.champion.winner);
+    return games;
   },
 };
 
